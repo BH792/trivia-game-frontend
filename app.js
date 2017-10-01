29de3e6b19befb1264c47fce9feca48785c8a839
Bobby Huang <zhenghuahuang@gmail.com>
2017-09-30 21:30:06 -0400

    basic route returning json from trivia api

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const https = require('https');
+const cors = require('cors');
+const app = express();
+
+
+const triviaURL = 'https://opentdb.com/api.php?'
+
+app.use(cors())
+
+app.get('/', function(req, res){
+  https.get(triviaURL + "amount=1", resp => {
+    resp.setEncoding("utf8");
+    let body = "";
+    resp.on("data", data => {
+      body += data;
+    });
+    resp.on("end", () => {
+      body = JSON.parse(body);
+      res.json(body);
+    });
+  });
+});
+
+app.listen(3000);
