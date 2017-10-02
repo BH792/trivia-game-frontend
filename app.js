49ec48eacc453df61956b5502d3ab262f3cb5c6b
Bobby Huang <zhenghuahuang@gmail.com>
2017-10-02 10:14:28 -0400

    websocket/redis integration

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const https = require('https');
 const cors = require('cors');
+const SocketServer = require('ws').Server;
+const cookieParser = require('cookie-parser')
+const redis = require('redis')
+
+const rPub = redis.createClient();
+const rSub = redis.createClient();
+
+rSub.on('message', function broadcast(channel, msg) {
+  if (LOBBY.length > 0) {
+    LOBBY.forEach(user => user.send(msg))
+  };
+});
+
 const app = express();
+const router = express.Router();
+const port = 3000;
 
+const triviaURL = 'https://opentdb.com/api.php?';
 
-const triviaURL = 'https://opentdb.com/api.php?'
+app.use( express.static( __dirname + '/public' ));
 
-app.use(cors())
-
-app.get('/', function(req, res){
+router.get('/question', function(req, res){
   https.get(triviaURL + "amount=1", resp => {
     resp.setEncoding("utf8");
     let body = "";
@@ -22,4 +36,76 @@ app.get('/', function(req, res){
   });
 });
 
-app.listen(3000);
+router.get('/', function(req, res) {
+  res.sendFile('/public/index.html', {root: __dirname })
+})
+
+app.use(cors());
+// app.use(cookieParser('this-is-a-super-secret-secret'))
+app.use('/', router)
+
+const server = app.listen(port, () => console.log('Listening on Port: ', port));
+
+const wss = new SocketServer({server: server});
+const LOBBY = [];
+// const GAMES = {};
+const wsRouter = {
+  getQuestion: getQuestion,
+  sendLobby: sendLobby,
+  sendAnswer: broadcastResult,
+};
+
+function getQuestion() {
+  https.get(triviaURL + "amount=1", resp => {
+    resp.setEncoding("utf8");
+    let body = "";
+    resp.on("data", data => {
+      body += data;
+    });
+    resp.on("end", () => {
+      let jsonRes = JSON.parse(body).results[0];
+      jsonRes.header = 'question';
+      rPub.publish('test-game', JSON.stringify(jsonRes));
+    });
+  });
+}
+
+function sendLobby() {
+  if (LOBBY.length > 0) {
+    let json = {
+      header: 'renderLobby',
+      users: []
+    };
+    LOBBY.forEach(ws => json.users.push(ws.user));
+    LOBBY.forEach(ws => ws.send(JSON.stringify(json)));
+  }
+}
+
+function broadcastResult(data) {
+  data.header = 'broadcastResult';
+  rPub.publish('test-game', JSON.stringify(data));
+}
+
+rSub.subscribe('test-game');
+wss.on('connection', function(ws, req) {
+  console.log('Client connected');
+  ws.user = req.url.slice(1, req.url.length);
+  ws.channels ? ws.channels.push('test-game') : ws.channels = ['test-game']
+  LOBBY.push(ws);
+  const index = LOBBY.indexOf(ws)
+
+  wsRouter.sendLobby()
+
+  ws.on('message', function routing(data) {
+    let dataJSON = JSON.parse(data)
+    if (wsRouter[dataJSON.header]) {
+      (wsRouter[dataJSON.header])(dataJSON)
+    }
+  })
+
+  ws.on('close', function disconnect() {
+    LOBBY.splice(index, 1)
+    wsRouter.sendLobby()
+    console.log('Client ', index ,' disconnected')
+  });
+});
---
+++ public/index.js
@@ -0,0 +1,103 @@
+document.querySelector('#username').addEventListener('submit', wsConnect)
+let questionDiv = document.querySelector('#question')
+let options = document.querySelector('#options')
+let userLobby = document.querySelector('#user-lobby')
+let results = document.querySelector('#results')
+
+let socket;
+let username;
+let currentQuestion;
+const url = 'http://192.168.3.106:3000'
+const wsurl = 'ws://192.168.3.106:3000'
+
+function checkAnswer() {
+  let result = this.innerHTML === currentQuestion.answer ? `${username} is Correct!` : `${username} is Wrong!`
+  alert(result)
+}
+
+function showQuestion(question) {
+  options.innerHTML = ""
+  currentQuestion = question
+  questionDiv.innerHTML = `<h2> ${currentQuestion.question} </h2>`
+  currentQuestion.choices.forEach( choice => {
+    let button = document.createElement('button')
+    button.className = "btn btn-outline-primary"
+    button.innerHTML = choice
+    button.addEventListener('click', checkAnswer)
+    options.appendChild(button)
+  })
+}
+
+function getQuestion() {
+  fetch(url + '/question')
+    .then(res => res.json())
+    .then(json => new Question(json.results[0]))
+    .then(question => showQuestion(question))
+}
+
+function getQuestionWS() {
+  socket.send(JSON.stringify({header: 'getQuestion'}))
+}
+
+function wsConnect(e) {
+  e.preventDefault()
+  username = e.target[0].value
+  socket = new WebSocket(wsurl + '/' + username)
+  socket.addEventListener('message', wsRouterClient)
+
+  let b = document.createElement('button')
+  b.className = "btn btn-outline-danger margin"
+  b.innerText = "Get Question"
+  b.addEventListener('click', getQuestionWS)
+  document.body.appendChild(b)
+  document.body.removeChild(e.currentTarget)
+}
+
+const wsRouter = {
+  renderLobby: renderLobby,
+  question: renderQuestion,
+  broadcastResult: renderResult
+}
+
+function wsRouterClient(msgEvent) {
+  let dataJSON = JSON.parse(msgEvent.data)
+  if (wsRouter[dataJSON.header]) {
+    (wsRouter[dataJSON.header])(dataJSON)
+  }
+}
+
+function renderLobby(json) {
+  userLobby.innerHTML = ''
+  let users = json.users
+  users.forEach(user => {
+    let li = document.createElement('li')
+    li.innerHTML = user
+    userLobby.appendChild(li)
+  })
+}
+
+function renderQuestion(json) {
+  let question = new Question(json)
+  options.innerHTML = ""
+  currentQuestion = question
+  questionDiv.innerHTML = `<h2> ${currentQuestion.question} </h2>`
+  currentQuestion.choices.forEach( choice => {
+    let button = document.createElement('button')
+    button.className = "btn btn-outline-primary answer-btn"
+    button.innerHTML = choice
+    button.addEventListener('click', checkAnswerWS)
+    options.appendChild(button)
+  })
+}
+
+function renderResult(json) {
+  let p = document.createElement('p')
+  p.innerText = json.result
+  results.appendChild(p)
+}
+
+function checkAnswerWS() {
+  let result = this.innerHTML === currentQuestion.answer ? `${username} is Correct!` : `${username} is Wrong!`
+  document.querySelectorAll('.answer-btn').forEach(btn => {btn.disabled = true})
+  socket.send(JSON.stringify({header: 'sendAnswer', result: result}))
+}
